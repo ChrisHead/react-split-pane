110c57dca42c0daa9920abe298025de496c016c3
Chris Head <chrishead90@hotmail.com>
2018-11-20 03:49:59 +0000

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
+import * as Prefixer from 'inline-style-prefixer';
 import * as React from 'react';
 
 export type Size = string | number;
@@ -18,6 +19,7 @@ export interface Props {
   onChange?: (newSize: number) => void;
   onResizerClick?: (event: MouseEvent) => void;
   onResizerDoubleClick?: (event: MouseEvent) => void;
+  // prefixer?: Prefixer;
   style?: React.CSSProperties;
   resizerStyle?: React.CSSProperties;
   paneStyle?: React.CSSProperties;
